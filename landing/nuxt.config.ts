07036a68214914679cbd3d227873d29d9affc328
Mariyam <llk665645@gmail.com>
2026-04-02 14:34:14 +0500

    Fix packa

--- landing/nuxt.config.ts
+++ landing/nuxt.config.ts
@@ -1,7 +1,14 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  // Это отключит проблемный движок
+  css: [],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   modules: [
     '@nuxt/image',
     '@nuxt/fonts',
