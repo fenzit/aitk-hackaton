234050771bea33a2589f954f751e364fe69293ae
Mariyam <llk665645@gmail.com>
2026-04-02 13:07:38 +0500

    000

--- landing/nuxt.config.ts
+++ landing/nuxt.config.ts
@@ -1,12 +1,18 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+import tailwindcss from "@tailwindcss/vite";
 
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  css: ['./app/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
   modules: [
     '@nuxt/a11y',
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/image'
   ]
-})
+});
