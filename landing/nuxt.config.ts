2735b8623adfa4ecc3aa97e22b1de231867eab88
Mariyam <llk665645@gmail.com>
2026-04-02 14:30:33 +0500

    Fix packaon

--- landing/nuxt.config.ts
+++ landing/nuxt.config.ts
@@ -1,21 +1,11 @@
-import tailwindcss from "@tailwindcss/vite";
-
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
-  future: {
-    compatibilityVersion: 4
-  },
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
-  },
   modules: [
-    '@nuxt/a11y',
-    '@nuxt/eslint',
+    '@nuxt/image',
     '@nuxt/fonts',
-    '@nuxt/image'
+    '@nuxt/eslint',
+    '@nuxt/a11y'
   ]
-});
+})
